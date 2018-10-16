6a7173ad52b3db6efe6b8757c2a5cb6463cce431
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2018-10-16 20:55:48 +0800

    Initial Commit

---
+++ prettier.config.js
@@ -0,0 +1,4 @@
+export const printWidth = 80;
+export const semi = true;
+export const singleQuote = true;
+export const trailingComma = 'all';
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () =>{
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  )
+};
+
+render(<App />, document.getElementById("root"));
